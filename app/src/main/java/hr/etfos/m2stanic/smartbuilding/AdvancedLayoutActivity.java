10a695f8e7152df43aecc466ff16334d67db3ffe
Mato Stanic <m2stanic@etfos.hr>
2016-04-19 20:02:18 +0200

    andvanced layout impl

--- app/src/main/java/hr/etfos/m2stanic/smartbuilding/AdvancedLayoutActivity.java
+++ app/src/main/java/hr/etfos/m2stanic/smartbuilding/AdvancedLayoutActivity.java
@@ -13,9 +13,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class AdvancedLayoutActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private RadioButton rbOn;
+    private RadioButton rbOff;
+    private EditText etHours;
+    private EditText etMinutes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +31,25 @@ public class AdvancedLayoutActivity extends AppCompatActivity
         setContentView(R.layout.activity_advanced_layout);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        rbOn = (RadioButton) findViewById(R.id.radioButton);
+        rbOff = (RadioButton) findViewById(R.id.radioButton2);
+        etHours = (EditText) findViewById(R.id.etHours);
+        etMinutes = (EditText) findViewById(R.id.etMinutes);
+
+        etHours.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                try {
+                    int val = Integer.parseInt(etHours.getText().toString());
+                    if (val < 0 || val > 24) {
+                        etHours.setText("");
+                        Toast.makeText(getApplicationContext(), "Vrijednost za sate mora biti od 0 do 24", Toast.LENGTH_LONG).show();
+                    }
+                } catch (NumberFormatException ex) {
+                }
+            }
+        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -41,6 +68,17 @@ public class AdvancedLayoutActivity extends AppCompatActivity
             drawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
+        }
+    }
+
+    public void rbClick(View v) {
+        int id = v.getId();
+        RadioButton rb = (RadioButton) findViewById(id);
+        if(rb.getId() == R.id.radioButton){
+            rbOff.setChecked(false);
+        }
+        else if (rb.getId() == R.id.radioButton2){
+            rbOn.setChecked(false);
         }
     }
 
@@ -70,4 +108,5 @@ public class AdvancedLayoutActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
 }
